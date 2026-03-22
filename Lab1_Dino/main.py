fc4f352e1d4024317a7b26604b4c56bc522f5cd8
Andrei Chervov <JWTHDYTWA@yandex.ru>
2026-03-23 00:46:21 +0700

    Finally, Lab 1

--- Lab1_Dino/main.py
+++ Lab1_Dino/main.py
@@ -1,9 +1,18 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torchvision
+import os
 
-model = torchvision.models.resnet18(pretrained=True)
+from torchvision.models.resnet import ResNet18_Weights
+
+model = torchvision.models.resnet18(weights=ResNet18_Weights.DEFAULT)
 layer4_features = None
 avgpool_emb = None
+
+lower_white = np.array([220, 220, 220], dtype=np.uint8)
+upper_white = np.array([255, 255, 255], dtype=np.uint8)
 
 def get_features(module, inputs, output):
     global layer4_features
@@ -13,18 +22,61 @@ def get_embedding(module, inputs, output):
     global avgpool_emb
     avgpool_emb = output
 
+def extract_feat(img: np.ndarray):
+    # img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    t = preprocess(img).unsqueeze(0)
+    with torch.no_grad():
+        model(t)
+    return (layer4_features.clone(), avgpool_emb.clone())
+
+def create_heatmap(obj_avgpool, img_layer4, image_w, image_h):
+    heatmap_tensor = 1 - torch.nn.functional.cosine_similarity(obj_avgpool, img_layer4).squeeze(0)
+    heatmap_min = heatmap_tensor.min()
+    heatmap_max = heatmap_tensor.max()
+    heatmap_norm = (heatmap_tensor - heatmap_min) / (heatmap_max - heatmap_min)
+    heatmap_np = heatmap_norm.numpy()
+
+    heatmap = cv2.resize(heatmap_np, (image_w, image_h), interpolation=cv2.INTER_LINEAR)
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    return heatmap
+
 preprocess = torchvision.transforms.Compose([
-    torchvision.transforms.ToPILImage(),
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
+root_dir = os.path.dirname(__file__)
+
 
 def main():
+    image = cv2.imread(os.path.join(root_dir, 'img/abc.jpg'))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    sample_dino = image[860:950,330:610].copy()
+    sample_dino[40:,200:] = [255,255,255]
+    mask = cv2.inRange(sample_dino, lower_white, upper_white)
+    sample_dino[mask == 255] = [0, 0, 0]
+
+    image = cv2.imread(os.path.join(root_dir, 'img/A_B_C.jpg'))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_h, image_w = image.shape[:2]
+
+    plt.imshow(sample_dino)
+    plt.show()
+
     model.layer4.register_forward_hook(get_features)
     model.avgpool.register_forward_hook(get_embedding)
     model.eval()
 
+    img_layer4, _ = extract_feat(image)
+    _, dino_avgpool = extract_feat(sample_dino)
+
+    heatmap = create_heatmap(dino_avgpool, img_layer4, image_w, image_h)
+    overlay = cv2.addWeighted(heatmap, 0.5, image, 0.5, 0)
+
+    plt.imshow(overlay)
+    plt.savefig(os.path.join(root_dir, 'fig1.jpg'))
+    plt.show()
 
 
 if __name__ == '__main__':
--- Lab2_Siamnet/main.py
+++ Lab2_Siamnet/main.py
@@ -1,32 +1,86 @@
 import os
 import cv2
 import random
-# import torch
+import torch
+import torch.nn as nn
+import torch.optim as optim
 import kagglehub
 
-class FaceDataset():
+from torch.utils.data import Dataset, DataLoader
+
+
+class FaceDataset(Dataset):
     def __init__(self, img_data, transform):
         super().__init__()
         self.crops = self.get_crops(img_data)
         self.transform = transform
     
-    def get_crops(self, data):
-        '''Load some img data'''
+    def make_pairs(self, data):
+        pairs = []
         return data
 
     def __getitem__(self, idx):
         img1, img2, label = self.crops[idx]
         input_img1 = self.transform(img1)
         input_img2 = self.transform(img2)
-        return input_img1, input_img2, label
+        return input_img1, input_img2, torch.tensor(label,dtype=torch.float32)
     
     def __len__(self):
         return len(self.crops)
 
+class SiamNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(1, 32, 3),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(32 * 110 * 90, 128)
+        )
+
+    def forward(self, img1, img2):
+        return self.feature_extractor(img1), self.feature_extractor(img2)
+
+class ContrastiveLoss(nn.Module):
+    def __init__(self, margin=1.0):
+        super().__init__()
+        self.margin = margin
+
+    def forward(self, out1, out2, label):
+        dist = torch.nn.functional.pairwise_distance(out1, out2)
+        loss = torch.mean(label * torch.pow(dist, 2) / 2 +
+                          (1 - label) * torch.pow(torch.clamp(self.margin - dist, min=0.0), 2) / 2)
+        return loss
+
+
+root_dir = os.path.dirname(__file__)
+
+
 def main():
-    path = kagglehub.dataset_download("kasikrit/att-database-of-faces", output_dir="./data")
+
+    path = kagglehub.dataset_download(
+        "kasikrit/att-database-of-faces",
+        output_dir=os.path.join(root_dir, 'data')
+    )
     
-    print(os.path.join(path, os.listdir(path)[2]))
+    faces = []
+    # Image size: 112x92
+    for subdir in os.listdir(path):
+        subdir_path = os.path.join(path, subdir)
+        if os.path.isdir(subdir_path) and subdir.startswith('s'):
+            label = int(subdir[1:])
+            print(label)
+            for img_name in os.listdir(subdir_path):
+                if img_name.endswith('.pgm'):
+                    img_path = os.path.join(subdir_path, img_name)
+                    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+                    faces.append((img, label))
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = SiamNet().to(device)
+    criterion = ContrastiveLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    train_loader = DataLoader(dataset, batch_size=32, shuffle=True)
 
 
 if __name__ == '__main__':
