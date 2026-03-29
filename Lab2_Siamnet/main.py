6b083bd38a61453b7279a4e3442332ad8f310995
Andrew Hearts <JWTHDYTWA@yandex.ru>
2026-03-29 13:21:49 +0700

    Sync

--- Lab2_Siamnet/main.py
+++ Lab2_Siamnet/main.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import numpy as np
 import random
 import torch
 import torch.nn as nn
@@ -10,14 +11,41 @@ from torch.utils.data import Dataset, DataLoader
 
 
 class FaceDataset(Dataset):
-    def __init__(self, img_data, transform):
+    def __init__(self, path, transform):
         super().__init__()
-        self.crops = self.get_crops(img_data)
+        images = []
+        class_labels = []
+        for subdir in os.listdir(path):
+            subdir_path = os.path.join(path, subdir)
+            if os.path.isdir(subdir_path) and subdir.startswith('s'):
+                class_label = int(subdir[1:])
+                # print(class_label)
+                for img_name in os.listdir(subdir_path):
+                    if img_name.endswith('.pgm'):
+                        img_path = os.path.join(subdir_path, img_name)
+                        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+                        
+                        images.append(img)
+                        class_labels.append(class_label)
+
+        self.pairs = self.make_pairs(images, class_labels)
         self.transform = transform
     
-    def make_pairs(self, data):
+    def make_pairs(self, images, class_labels):
+        images_np = np.array(images)
+        labels_np = np.array(class_labels)
+        labels_unique = np.unique(labels_np)
         pairs = []
-        return data
+
+        for class_label in labels_unique:
+
+            for image in images_np[labels_np == class_label]:
+                positive = ...
+
+                pairs.append((image, positive, 1.0))
+                ...
+            ...
+        print(labels_unique)
 
     def __getitem__(self, idx):
         img1, img2, label = self.crops[idx]
@@ -48,8 +76,9 @@ class ContrastiveLoss(nn.Module):
 
     def forward(self, out1, out2, label):
         dist = torch.nn.functional.pairwise_distance(out1, out2)
-        loss = torch.mean(label * torch.pow(dist, 2) / 2 +
-                          (1 - label) * torch.pow(torch.clamp(self.margin - dist, min=0.0), 2) / 2)
+        loss = torch.mean(
+            label * torch.pow(dist, 2) / 2 +
+            (1 - label) * torch.pow(torch.clamp(self.margin - dist, min=0.0), 2) / 2)
         return loss
 
 
@@ -63,24 +92,14 @@ def main():
         output_dir=os.path.join(root_dir, 'data')
     )
     
-    faces = []
+    dataset = D=FaceDataset(path, None)
     # Image size: 112x92
-    for subdir in os.listdir(path):
-        subdir_path = os.path.join(path, subdir)
-        if os.path.isdir(subdir_path) and subdir.startswith('s'):
-            label = int(subdir[1:])
-            print(label)
-            for img_name in os.listdir(subdir_path):
-                if img_name.endswith('.pgm'):
-                    img_path = os.path.join(subdir_path, img_name)
-                    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-                    faces.append((img, label))
     
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = SiamNet().to(device)
-    criterion = ContrastiveLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    train_loader = DataLoader(dataset, batch_size=32, shuffle=True)
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # model = SiamNet().to(device)
+    # criterion = ContrastiveLoss()
+    # optimizer = optim.Adam(model.parameters(), lr=0.001)
+    # train_loader = DataLoader(dataset, batch_size=32, shuffle=True)
 
 
 if __name__ == '__main__':
